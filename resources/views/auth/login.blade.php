d106abc444693e7a321c2e7c6e8a503c50968835
trangtt <trangtran@atmarkcafe.org>
2017-02-28 15:42:59 +0700

    remove btn login with tweeter

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,6 @@
                     <div class="form-group">
                         <div class="col-md-6 col-md-offset-4">
                             <a href="{{ url('/auth/google') }}" class="btn btn-github"><i class="fa fa-google"></i> Google</a>
-                            <a href="{{ url('/auth/twitter') }}" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
                             <a href="{{ url('/auth/facebook') }}" class="btn btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
                         </div>
                     </div>
